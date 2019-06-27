7d25b52578504d274d733e9f43af6f1ef2c17f6e
Aleksandar Sadzak <sadzak23@gmail.com>
2019-06-27 23:00:00 +0200

    5x5 - Added dynamic exercises names and values

--- src/components/5x5/Workout5x5.js
+++ src/components/5x5/Workout5x5.js
@@ -11,11 +11,12 @@ export default class Workout5x5 extends React.Component {
         squat: 120,
         benchPress: 95,
         barbellRow: 80,
-        overHeadPress: 60,
+        overheadPress: 60,
         deadLift: 110,
         bodyWeight: 75
       },
       workout: {
+        type: 'a',
         exercise1: {
           set1: 0,
           set2: 0,
@@ -67,55 +68,59 @@ export default class Workout5x5 extends React.Component {
   /* Exercise 1 */
   onRepCountE1R1 = () => {
     this.onRepCount('exercise1', 'set1')
-  }
+  };
   onRepCountE1R2 = () => {
     this.onRepCount('exercise1', 'set2')
-  }
+  };
   onRepCountE1R3 = () => {
     this.onRepCount('exercise1', 'set3')
-  }
+  };
   onRepCountE1R4 = () => {
     this.onRepCount('exercise1', 'set4')
-  }
+  };
   onRepCountE1R5 = () => {
     this.onRepCount('exercise1', 'set5')
-  }
+  };
 
   /* Exercise 2 */
   onRepCountE2R1 = () => {
     this.onRepCount('exercise2', 'set1')
-  }
+  };
   onRepCountE2R2 = () => {
     this.onRepCount('exercise2', 'set2')
-  }
+  };
   onRepCountE2R3 = () => {
     this.onRepCount('exercise2', 'set3')
-  }
+  };
   onRepCountE2R4 = () => {
     this.onRepCount('exercise2', 'set4')
-  }
+  };
   onRepCountE2R5 = () => {
     this.onRepCount('exercise2', 'set5')
-  }
+  };
 
   /* Exercise 3 */
   onRepCountE3R1 = () => {
     this.onRepCount('exercise3', 'set1')
-  }
+  };
   onRepCountE3R2 = () => {
     this.onRepCount('exercise3', 'set2')
-  }
+  };
   onRepCountE3R3 = () => {
     this.onRepCount('exercise3', 'set3')
-  }
+  };
   onRepCountE3R4 = () => {
     this.onRepCount('exercise3', 'set4')
-  }
+  };
   onRepCountE3R5 = () => {
     this.onRepCount('exercise3', 'set5')
-  }
+  };
 
   render() {
+    const exercise2Name = this.state.workout.type === 'a' ? "Bench Press" : "Overhead Press";
+    const exercise2Weight = this.state.workout.type === 'a' ? this.state.userData.benchPress : this.state.userData.overheadPress;
+    const exercise3Name = this.state.workout.type === 'a' ? "Barbell Row" : "Dead Lift";
+    const exercise3Weight = this.state.workout.type === 'a' ? this.state.userData.barbellRow : this.state.userData.deadLift;
     return (
       <div className="content-container">
         <Link to="/dashboard5x5">
@@ -123,15 +128,20 @@ export default class Workout5x5 extends React.Component {
             <FontAwesomeIcon icon={faTimes} className="timer-x" />
           </button>
         </Link>
-        <div className="timer5x5">
-          <h1>Rest: 01:00</h1>
+        <div className="header5x5">
+          <div className="timer5x5">
+            <h1>Rest: 01:00</h1>
+          </div>
+          <div className="workout-bodyweight">
+            <h2>Body Weight: {this.state.userData.bodyWeight}Kg</h2>
+          </div>
         </div>
 
         {/* Exercise 1 */}
         <div className="workout-exercise">
           <div className="exercise-title-5x5">
             <h2>Squat</h2>
-            <h3>5x5 {this.state.userData.squat}kg</h3>
+            <h2>5x5 {this.state.userData.squat}kg</h2>
           </div>
           <div className="counter5x5">
             <button onClick={this.onRepCountE1R1} className="btn-exercise-count">{this.state.workout.exercise1.set1}</button>
@@ -145,8 +155,8 @@ export default class Workout5x5 extends React.Component {
         {/* Exercise 2 */}
         <div className="workout-exercise">
           <div className="exercise-title-5x5">
-            <h2>Bench Press</h2>
-            <h3>5x5 {this.state.userData.benchPress}kg</h3>
+            <h2>{exercise2Name}</h2>
+            <h2>5x5 {exercise2Weight}kg</h2>
           </div>
           <div className="counter5x5">
             <button onClick={this.onRepCountE2R1} className="btn-exercise-count">{this.state.workout.exercise2.set1}</button>
@@ -160,8 +170,8 @@ export default class Workout5x5 extends React.Component {
         {/* Exercise 3 */}
         <div className="workout-exercise">
           <div className="exercise-title-5x5">
-            <h2>Barbell Row</h2>
-            <h3>5x5 {this.state.userData.barbellRow}kg</h3>
+            <h2>{exercise3Name}</h2>
+            <h2>5x5 {exercise3Weight}kg</h2>
           </div>
           <div className="counter5x5">
             <button onClick={this.onRepCountE3R1} className="btn-exercise-count">{this.state.workout.exercise3.set1}</button>
@@ -170,10 +180,6 @@ export default class Workout5x5 extends React.Component {
             <button onClick={this.onRepCountE3R4} className="btn-exercise-count">{this.state.workout.exercise3.set4}</button>
             <button onClick={this.onRepCountE3R5} className="btn-exercise-count">{this.state.workout.exercise3.set5}</button>
           </div>
-        </div>
-
-        <div className="workout-bodyweight">
-          <h2>Body Weight: {this.state.userData.bodyWeight}Kg</h2>
         </div>
       </div>
     )
