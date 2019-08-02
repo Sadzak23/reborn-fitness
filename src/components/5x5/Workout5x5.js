c4bad5da905105c5c9255ef76389569839325739
Aleksandar Sadzak <sadzak23@gmail.com>
2019-08-02 04:08:11 +0200

    Added Workout5x5 Rest timer

--- src/components/5x5/Workout5x5.js
+++ src/components/5x5/Workout5x5.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { history } from '../../routers/AppRouter';
+import { formatSeconds } from '../Format';
 
 export class Workout5x5 extends React.Component {
   constructor(props) {
@@ -40,10 +41,53 @@ export class Workout5x5 extends React.Component {
           set4: 0,
           set5: 0,
         },
-      }
-    }
-  }
+      },
+      // Timer state
+      interval: null,
+      miliseconds: 0,
+      paused: false,
 
+    }
+  };
+
+  ///////////////// Timer //////////////////////////////////////////
+
+  tick = () => {
+    this.setState({ miliseconds: this.state.miliseconds -= 100 })
+    // End Timer
+    if (this.state.miliseconds <= 0) {
+      clearInterval(this.state.interval);
+
+      // Play sound on 0
+      new Audio('../assets/beep0.mp3').play();
+      this.setState({
+        paused: true
+      });
+    };
+    // Play sound on 3,2,1
+    if (this.state.miliseconds === 3000 || this.state.miliseconds === 2000 || this.state.miliseconds === 1000) {
+      new Audio('../assets/beep.mp3').play();
+    };
+    // Play sound on 10
+    if (this.state.miliseconds === 10000) {
+      new Audio('../assets/beep.mp3').play();
+    };
+  };
+
+  togglePause = () => {
+    this.setState({ paused: !this.state.paused });
+    if (!this.state.paused) {
+      clearInterval(this.state.interval);
+    } else {
+      this.setState({ interval: setInterval(this.tick, 100) });
+    }
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  };
+
+  /////////////////////////////////////////////////////////////////////////////
   onRepCount = (exerciseNo, setNo) => {
     if (this.state.workout[exerciseNo][setNo] === 0) {
       this.setState({
@@ -67,76 +111,99 @@ export class Workout5x5 extends React.Component {
       });
     }
   };
+  runTimer = () => {
+    clearInterval(this.state.interval);
+    this.setState({
+      miliseconds: 60000
+    }, () => {
+      this.setState({
+        interval: setInterval(this.tick, 100)
+      });
+    });
+  };
   /* Exercise 1 */
   onRepCountE1R1 = () => {
-    this.onRepCount('exercise1', 'set1')
+    this.onRepCount('exercise1', 'set1');
+    this.runTimer();
   };
   onRepCountE1R2 = () => {
-    this.onRepCount('exercise1', 'set2')
+    this.onRepCount('exercise1', 'set2');
+    this.runTimer();
   };
   onRepCountE1R3 = () => {
-    this.onRepCount('exercise1', 'set3')
+    this.onRepCount('exercise1', 'set3');
+    this.runTimer();
   };
   onRepCountE1R4 = () => {
-    this.onRepCount('exercise1', 'set4')
+    this.onRepCount('exercise1', 'set4');
+    this.runTimer();
   };
   onRepCountE1R5 = () => {
-    this.onRepCount('exercise1', 'set5')
+    this.onRepCount('exercise1', 'set5');
+    this.runTimer();
   };
 
   /* Exercise 2 */
   onRepCountE2R1 = () => {
-    this.onRepCount('exercise2', 'set1')
+    this.onRepCount('exercise2', 'set1');
+    this.runTimer();
   };
   onRepCountE2R2 = () => {
-    this.onRepCount('exercise2', 'set2')
+    this.onRepCount('exercise2', 'set2');
+    this.runTimer();
   };
   onRepCountE2R3 = () => {
-    this.onRepCount('exercise2', 'set3')
+    this.onRepCount('exercise2', 'set3');
+    this.runTimer();
   };
   onRepCountE2R4 = () => {
-    this.onRepCount('exercise2', 'set4')
+    this.onRepCount('exercise2', 'set4');
+    this.runTimer();
   };
   onRepCountE2R5 = () => {
-    this.onRepCount('exercise2', 'set5')
+    this.onRepCount('exercise2', 'set5');
+    this.runTimer();
   };
 
   /* Exercise 3 */
   onRepCountE3R1 = () => {
-    this.onRepCount('exercise3', 'set1')
+    this.onRepCount('exercise3', 'set1');
+    this.runTimer();
   };
   onRepCountE3R2 = () => {
-    this.onRepCount('exercise3', 'set2')
+    this.onRepCount('exercise3', 'set2');
+    this.runTimer();
   };
   onRepCountE3R3 = () => {
-    this.onRepCount('exercise3', 'set3')
+    this.onRepCount('exercise3', 'set3');
+    this.runTimer();
   };
   onRepCountE3R4 = () => {
-    this.onRepCount('exercise3', 'set4')
+    this.onRepCount('exercise3', 'set4');
+    this.runTimer();
   };
   onRepCountE3R5 = () => {
-    this.onRepCount('exercise3', 'set5')
+    this.onRepCount('exercise3', 'set5');
+    this.runTimer();
   };
 
-  render() {
-    //Exit workout confirmation
-    const onExit = () => swal({
-      title: "You're not a quitter!",
-      text: "Are you?!",
-      icon: "warning",
-      buttons: {
-        cancel: "Cancel",
-        exit: {
-          text: "Yes, I quit!",
-          value: "exit",
-          className: "btn-alert-exit"
-        },
+  onExit = () => swal({
+    title: "You're not a quitter!",
+    text: "Are you?!",
+    icon: "warning",
+    buttons: {
+      cancel: "Cancel",
+      exit: {
+        text: "Yes, I quit!",
+        value: "exit",
+        className: "btn-alert-exit"
       },
-    })
-      .then((value) =>
-        value === "exit" && history.push("/")
-      );
+    },
+  }).then((value) =>
+      value === "exit" && history.push("/")
+    );
 
+  render() {
     const exercise2Name = this.state.workout.type === 'a' ? "Bench Press" : "Overhead Press";
     const exercise2Weight = this.state.workout.type === 'a' ? this.state.userData.benchPress : this.state.userData.overheadPress;
     const exercise3Name = this.state.workout.type === 'a' ? "Barbell Row" : "Dead Lift";
@@ -144,7 +211,7 @@ export class Workout5x5 extends React.Component {
 
     return (
       <div className="content-container">
-        <button className="btn-x" onClick={onExit}>
+        <button className="btn-x" onClick={this.onExit}>
           <FontAwesomeIcon icon={faTimes} className="timer-x" />
         </button>
 
@@ -153,7 +220,7 @@ export class Workout5x5 extends React.Component {
             <h2>Change Weights</h2>
           </Link>
           <div className="timer5x5">
-            <h1>Rest: 01:00</h1>
+            <h1>Rest: {formatSeconds(Math.ceil(this.state.miliseconds / 1000))}</h1>
           </div>
           <div className="workout-bodyweight">
             <h2>Body Weight: {this.state.userData.bodyWeight}Kg</h2>
