6045c8224cc227b203d39987464bf0db8f39037a
Aleksandar Sadzak <sadzak23@gmail.com>
2019-08-20 10:01:20 +0200

    Add new timer colors

--- src/components/ColorMap.js
+++ src/components/ColorMap.js
@@ -1,8 +1,10 @@
 export const colorMap = { 
-  "#63d313" : "Green",
-  "#2bd99f" : "Mint",
+  "#2bd99f" : "Green",
+  "#e3da86" : "Yellow",
   "#1dc4f2" : "Blue",
   "#d45c5c" : "Red",
+  "#c28bcb" : "Pink",
+  "#bc9c90" : "Brown",
   "#cccccc" : "Gray",
 }
 export const colorMapGender = {
