a22d97e2ce73866836c939280bfc4f1ba6d7206a
Aleksandar Sadzak <sadzak23@gmail.com>
2019-09-21 01:19:53 +0200

    5x5 - Add color validation for exercise done

--- src/components/5x5/Exercise5x5.js
+++ src/components/5x5/Exercise5x5.js
@@ -2,20 +2,28 @@ import React from 'react';
 
 export const Exercise5x5 = ({ exerciseName, exerciseWeight, exerciseNo, exerciseSets, onRepCount }) => {
   const done = () => {
+    const element = document.getElementById(exerciseName)
     if (exerciseSets.done === "5x5") {
+      element.classList.remove('almost');
+      element.classList.add('done');
       return (
         <h3>Great job! Next time lift {(exerciseWeight + 2.5)}kg</h3>
       )
     } else if (exerciseSets.done === "almost") {
+      element.classList.remove('done');
+      element.classList.add('almost');
       return (
         <h3>It's ok! You'll get it next time.</h3>
       )
+    } else {
+      element.classList.remove('done');
+      element.classList.remove('almost');
     }
   }
 
   const sets = [1, 2, 3, 4, 5]
   return (
-    <div className="workout-exercise">
+    <div id={exerciseName} className="workout-exercise">
       <div className="exercise-title-5x5">
         <h2>{exerciseName}</h2>
         {exerciseSets.done && done()}
@@ -23,7 +31,7 @@ export const Exercise5x5 = ({ exerciseName, exerciseWeight, exerciseNo, exercise
       </div>
       <div className="counter5x5">
         {sets.map((set) => (
-          <button onClick={() => {onRepCount(exerciseNo, `set${set}`)}} className="btn-exercise-count" key={exerciseName + set}>{exerciseSets[`set${set}`]}</button>
+          <button onClick={() => { onRepCount(exerciseNo, `set${set}`) }} className="btn-exercise-count" key={exerciseName + set}>{exerciseSets[`set${set}`]}</button>
         ))}
       </div>
     </div>
