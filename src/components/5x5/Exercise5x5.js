d348d219d3f3bdd25f8093b153a95681fc68fcb6
Aleksandar Sadzak <sadzak23@gmail.com>
2019-09-21 03:25:41 +0200

    5x5 - Better way of exercise done validation

--- src/components/5x5/Exercise5x5.js
+++ src/components/5x5/Exercise5x5.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export const Exercise5x5 = ({ exerciseName, exerciseWeight, exerciseNo, exerciseSets, onRepCount }) => {
-  const done = () => {
+
+  const sets = [1, 2, 3, 4, 5]
+
+  useEffect(() => {
     const element = document.getElementById(exerciseName)
     if (exerciseSets.done === "5x5") {
       element.classList.remove('almost');
       element.classList.add('done');
-      return (
-        <h3>Great job! Next time lift {(exerciseWeight + 2.5)}kg</h3>
-      )
     } else if (exerciseSets.done === "almost") {
       element.classList.remove('done');
       element.classList.add('almost');
-      return (
-        <h3>It's ok! You'll get it next time.</h3>
-      )
     } else {
       element.classList.remove('done');
       element.classList.remove('almost');
     }
-  }
+  });
 
-  const sets = [1, 2, 3, 4, 5]
   return (
     <div id={exerciseName} className="workout-exercise">
       <div className="exercise-title-5x5">
         <h2>{exerciseName}</h2>
-        {exerciseSets.done && done()}
+        {exerciseSets.done === "5x5" && <h3>Great job! Next time lift {(exerciseWeight + 2.5)}kg</h3>}
+        {exerciseSets.done === "almost" && <h3>It's ok! You'll get it next time.</h3>}
         <h2>5x5 {exerciseWeight}kg</h2>
       </div>
       <div className="counter5x5">
