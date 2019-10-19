4e356622d265fb74120820000e5c32bf9b7c6d0e
Aleksandar Sadzak <sadzak23@gmail.com>
2019-10-19 02:34:38 +0200

    Change SingleTimer info to exercise & round count

--- src/components/timers/SingleTimer.js
+++ src/components/timers/SingleTimer.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { onRemoveAlert } from '../Alerts'
+import { onRemoveAlert } from '../Alerts';
 import { startRemoveTimer } from '../../actions/timers';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-export const SingleTimer = ({ id, name, warmupTime, intervals, startRemoveTimer }) => {
+export const SingleTimer = ({ id, name, rounds, intervals, startRemoveTimer }) => {
   // Delete timer confirmation
   const onRemoveTimer = () => onRemoveAlert(startRemoveTimer, id, name);
-  
+  const exerciseNo = intervals.reduce((count, interval) => interval.intervalType === "exercise" ? count + 1 : count, 0)
   return (
     <div className="list-item">
       <div className="list-int list-userName">
         <Link to={`/timer/${id}`} className="btn-activate grid" onMouseDown={(e) => { e.preventDefault() }}>
           <h3>{name}</h3>
-          <p>{intervals.length} intervals - Warmup time: {warmupTime}</p>
+          <p>{exerciseNo} exercises - {rounds} rounds</p>
         </Link>
       </div>
 
