d4a3b74f5740a15fe26084e6b027927e7d9d7aff
Aleksandar Sadzak <sadzak23@gmail.com>
2019-12-24 03:49:08 +0100

    Timer use date.now. Make it more accurate

--- src/components/timers/Timer.js
+++ src/components/timers/Timer.js
@@ -15,10 +15,11 @@ class Timer extends React.Component {
         intervalNo: -1,
         paused: true,
         phase: "Warmup",
-        miliseconds: props.timer.warmupTime * 1000,
         seconds: props.timer.warmupTime,
+        totalSeconds: props.timer.warmupTime,
         type: "warmup",
-        timerRun: null
+        timerRun: null,
+        timeStamp: null
       }
     } else {
       this.state = {
@@ -26,10 +27,12 @@ class Timer extends React.Component {
         intervalNo: 0,
         paused: true,
         phase: props.timer.intervals[0].intervalName,
-        miliseconds: props.timer.intervals[0].intervalSec * 1000 + props.timer.intervals[0].intervalMin * 60000,
         seconds: props.timer.intervals[0].intervalSec + props.timer.intervals[0].intervalMin * 60,
+        remainingSeconds: props.timer.intervals[0].intervalSec + props.timer.intervals[0].intervalMin * 60,
+        totalSeconds: props.timer.intervals[0].intervalSec + props.timer.intervals[0].intervalMin * 60,
         type: props.timer.intervals[0].intervalType,
-        timerRun: null
+        timerRun: null,
+        timeStamp: null
       }
     };
   };
@@ -58,35 +61,56 @@ class Timer extends React.Component {
   longBeep = new Audio('../assets/beep0.mp3');
 
   tick = () => {
-    this.setState({ miliseconds: this.state.miliseconds -= 100 });
+    this.setState({
+      seconds: this.state.totalSeconds - Math.round((Date.now() - this.state.timeStamp) / 1000)
+    })
     //Next interval
-    if (this.state.miliseconds === 0) {
+    if (this.state.seconds <= 0) {
       this.handleNext();
       this.longBeep.play();  // Play sound on 0
       // Last interval
       if (this.state.intervalNo + 1 === this.intervals.length) {
-        if (this.state.miliseconds === 0) {
-          clearInterval(this.state.timerRun);
-          this.setState({ paused: true });
+        if (this.state.seconds <= 0) {
+          this.onStopTimer();
           this.longBeep.play();  // Play sound on END TIMER
-        }
+        };
       };
     };
     // Play sound on 3,2,1
-    (this.state.miliseconds === 3000 || this.state.miliseconds === 2000 || this.state.miliseconds === 1000) && this.beep.play();
+    (this.state.seconds === 3 || this.state.seconds === 2 || this.state.seconds === 1) && this.beep.play();
     // Play sound on 10
-    (this.state.miliseconds === 10000 && this.state.seconds >= 15) && this.beep.play();
+    (this.state.seconds === 10 && this.state.totalSeconds >= 15) && this.beep.play();
+  };
+
+  togglePause = () => {
+    this.state.paused ? this.onRunTimer() : this.onStopTimer();
+  };
+
+  onRunTimer = () => {
+    this.setState({
+      paused: false,
+      timerRun: setInterval(this.tick, 1000),
+      timeStamp: Date.now()
+    })
+  };
+  onStopTimer = () => {
+    clearInterval(this.state.timerRun)
+    this.setState({ paused: true, totalSeconds: this.state.seconds })
   };
 
   handleNext = () => {
     if (this.state.intervalNo + 1 < this.intervals.length) {
+      if (!this.state.paused) {
+        this.onStopTimer()
+        this.onRunTimer()
+      }
       this.setState({
         color: this.intervals[this.state.intervalNo + 1].intervalColor,
         phase: this.intervals[this.state.intervalNo + 1].intervalName,
-        miliseconds: this.intervals[this.state.intervalNo + 1].intervalSec * 1000 + this.intervals[this.state.intervalNo + 1].intervalMin * 60000,
         seconds: this.intervals[this.state.intervalNo + 1].intervalSec + this.intervals[this.state.intervalNo + 1].intervalMin * 60,
+        totalSeconds: this.intervals[this.state.intervalNo + 1].intervalSec + this.intervals[this.state.intervalNo + 1].intervalMin * 60,
         type: this.intervals[this.state.intervalNo + 1].intervalType,
-        intervalNo: this.state.intervalNo + 1
+        intervalNo: this.state.intervalNo + 1,
       });
     };
   };
@@ -97,23 +121,13 @@ class Timer extends React.Component {
       this.setState({
         color: interval.intervalColor,
         phase: interval.intervalName,
-        miliseconds: interval.intervalSec * 1000 + interval.intervalMin * 60000,
         seconds: interval.intervalSec + interval.intervalMin * 60,
+        totalSeconds: interval.intervalSec + interval.intervalMin * 60,
         type: interval.intervalType,
-        intervalNo: this.state.intervalNo - 1
+        intervalNo: this.state.intervalNo - 1,
+        timeStamp: Date.now()
       });
     };
-  };
-
-  togglePause = () => {
-    this.setState({
-      paused: !this.state.paused
-    });
-    if (!this.state.paused) {
-      clearInterval(this.state.timerRun);
-    } else {
-      this.setState({ timerRun: setInterval(this.tick, 100) });
-    }
   };
 
   componentDidMount() {
@@ -138,18 +152,19 @@ class Timer extends React.Component {
     const elapsedTime = this.intervals.slice(
       0, this.state.intervalNo + 1).reduce(
         (time, interval) => interval.intervalSec + interval.intervalMin * 60 + time, 0) -
-      Math.ceil(this.state.miliseconds / 1000);
+      Math.ceil(this.state.seconds);
     //Remaining time
     const remainingTime = this.intervals.reduce((time, interval) => interval.intervalSec + interval.intervalMin * 60 + time, 0) - elapsedTime;
 
     return (
+      //<button className="btn-x" onClick={onExit}>
       <div>
         <button className="btn-x" onClick={onExit}>
           <FontAwesomeIcon icon={faTimes} className="timer-x" />
         </button>
         <div className="content-container timer-container">
           <div>
-            <h1 className="timer-clock">{formatSeconds(Math.ceil(this.state.miliseconds / 1000))}</h1>
+            <h1 className="timer-clock">{formatSeconds(Math.ceil(this.state.seconds))}</h1>
             <h3 className="timer-interval-name">- {this.state.phase} -</h3>
           </div>
           <div>
